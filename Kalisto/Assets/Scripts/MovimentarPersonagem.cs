3f2216e7f0392cc28dd4e40133253f8a731a546d
Fabio-Frare <nabata99@hotmail.com>
2023-11-09 20:19:08 -0300

    Commit  hero moviment

---
+++ Kalisto/Assets/Scripts/MovimentarPersonagem.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentarPersonagem : MonoBehaviour
+{
+    public CharacterController controle;
+    public float velocidade = 6f;
+    public float alturaPulo = 6f;
+    public float gravidade = -20f;
+
+    public Transform checaChao;
+    public float raioEsfera = 0.4f;
+    public LayerMask chaoMask;
+    public bool estaNoChao;
+
+    Vector3 velocidadeCai;
+
+    void Start()
+    {
+        controle = GetComponent<CharacterController>();        
+    }
+
+    void Update()
+    {
+        estaNoChao = Physics.CheckSphere(checaChao.position, raioEsfera, chaoMask);
+
+        float x = Input.GetAxis("Horizontal");       
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 mover = transform.right * x + transform.forward * z;
+
+        controle.Move(mover * velocidade * Time.deltaTime);    
+
+        if (estaNoChao && Input.GetButtonDown("Jump"))
+        {
+            velocidadeCai.y = Mathf.Sqrt(alturaPulo * -2f * gravidade);
+        }   
+
+        if (!estaNoChao)
+        {
+            velocidadeCai.y += gravidade * Time.deltaTime;
+        }
+
+        controle.Move(velocidadeCai * Time.deltaTime);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(checaChao.position, raioEsfera);
+    }
+    
+    
+}
