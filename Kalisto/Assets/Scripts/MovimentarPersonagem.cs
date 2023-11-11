48cff6ea6502087e3992905dc775ac5a803999b4
Fabio-Frare <nabata99@hotmail.com>
2023-11-11 16:23:00 -0300

    Commit Kate movment walk, jump and run

--- Kalisto/Assets/Scripts/MovimentarPersonagem.cs
+++ Kalisto/Assets/Scripts/MovimentarPersonagem.cs
@@ -6,9 +6,9 @@ public class MovimentarPersonagem : MonoBehaviour
 {
     public CharacterController controle;
     public float velocidade = 6f;
-    public float alturaPulo = 6f;
-    public float gravidade = -20f;
-
+    public float alturaPulo = 1f;
+    public float gravidade = -40f;
+ 
     public Transform checaChao;
     public float raioEsfera = 0.4f;
     public LayerMask chaoMask;
@@ -34,7 +34,7 @@ public class MovimentarPersonagem : MonoBehaviour
 
         controle.Move(mover * velocidade * Time.deltaTime);    
 
-        /*
+        
         if (estaNoChao && Input.GetButtonDown("Jump"))
         {
             velocidadeCai.y = Mathf.Sqrt(alturaPulo * -2f * gravidade);
@@ -45,7 +45,17 @@ public class MovimentarPersonagem : MonoBehaviour
             velocidadeCai.y += gravidade * Time.deltaTime;
         }
 
-        controle.Move(velocidadeCai * Time.deltaTime);*/
+        controle.Move(velocidadeCai * Time.deltaTime);
+
+        // Enquanto Shift estiver pressionado, dobra a velocidade simulando uma corrida.
+        if (estaNoChao && Input.GetKey(KeyCode.LeftShift))
+        {
+            velocidade = 12f;
+        } else
+        {
+            velocidade = 6f;
+        }
+       
     }
 
    void OnDrawGizmosSelected()
